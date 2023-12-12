574d668fb867b8fe56689e55897ec237b1d568c3
AdamGitDotNet <adamleon@tlen.pl>
2023-12-12 22:03:30 +0100

    Add project files.

---
+++ zadanie_4_11/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace zadania
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool liczbaParzysta = false;
+            try
+            {
+                Console.WriteLine("Podaj liczbę");
+                do
+                {
+                    if (Int32.TryParse(Console.ReadLine(), out int wynik))
+                    {
+                        if (wynik % 2 == 0)
+                        {
+                            Console.WriteLine("Liczba parzysta");
+                            liczbaParzysta = true;
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Liczba nieparzysta, podaj inna liczbę");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Podałeś niepoprawne dane. Spróbuj jeszcze raz");
+                    }
+                }
+
+                while (!liczbaParzysta);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
